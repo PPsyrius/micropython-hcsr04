d1b1db35c7e748f452c9ebaeb83324e83aa305ad
PPsyrius <PPsyrius@users.noreply.github.com>
2021-02-25 00:36:51 +0700

    Incorporate air temp, library optimisation, inches unit support
    Co-Authored-By: trimalcione <17990410+trimalcione@users.noreply.github.com>
    Co-Authored-By: Juan Fernando Donadío <50814756+jfdona23@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,25 +1,23 @@
 # HC-SR04 Sensor driver in micropython
 
-Micropython driver for the well-known untrasonic sensor [HC-SR04](https://www.mpja.com/download/hc-sr04_ultrasonic_module_user_guidejohn.pdf)
+Micropython driver for the well-known ultrasonic sensor [HC-SR04](https://www.mpja.com/download/hc-sr04_ultrasonic_module_user_guidejohn.pdf)
 
-The driver has been tested on Wemos D1 mini PRO, but It should work on whatever other micropython board, 
-if anyone find problems in other boards, please open an issue and we'll see.
+This improved version of the original library (https://github.com/rsc1975/micropython-hcsr04) has only been tested on Raspberry Pi Pico so far.
 
 ## Motivation
 
-The existing drivers in micropython are a bit old and they don't use the relatively new method `machine.time_pulse_us()` which
-Is more accurate that whatever other method using pure python, besides the code is compliant with "standard" micropython,
-there is no code for specific boards.
+The original's maintainer for this library, @rsc1975 hasn't been active for a while. New changes are thus incorporated here instead from the existing repo, this includes:
+- Replacing `time` with `utime` for improve efficiency, also only imported needed functions rather than the entire module - as per spotted by @jfdona23
+- The incorporation of air temperature calculation (this affects the speed of sound, and thus, the distance) by @trimalcione
+- Miscellaneous overall improvements such as `update_air_temp(temp)` and `distance_in()`
 
-Finally I've added a method, `distance_mm()` that don't use floating point operations, for environments where there is no
-floating point capabilities.
+The improved version's `distance_mm()` no longer works for environments where there is no floating point capabilities, and thus forked accordingly from the original repo.
 
 ## Examples of use:
 
 ### How to get the distance
 
 The `distance_cm()` method returns a `float` with the distance measured by the sensor.
-
 ```python
 from hcsr04 import HCSR04
 
@@ -30,17 +28,29 @@ distance = sensor.distance_cm()
 print('Distance:', distance, 'cm')
 ```
 
-There is another method, `distance_mm()`, that returns the distance in milimeters (`int` type) and **no floating point is used**, designed 
-for environments that doesn't support floating point operations.
-
+The `distance_mm()` method  returns the distance in milimeters (`int` type); unlike the original library this no longer works with environment with no floating point capabilities from the usage of `sqrt` function for air temperature calculations.
 ```python
+from hcsr04 import HCSR04
+
+sensor = HCSR04(trigger_pin=16, echo_pin=0)
+
 distance = sensor.distance_mm()
 
 print('Distance:', distance, 'mm')
 ```
-The default timeout is based on the sensor limit (4m), but we can also define a different timeout, 
-passing the new value in microseconds to the constructor.
 
+The `distance_in()` method  returns the distance in inches (`float` type).
+```python
+from hcsr04 import HCSR04
+
+sensor = HCSR04(trigger_pin=16, echo_pin=0)
+
+distance = sensor.distance_in()
+
+print('Distance:', distance, 'in')
+```
+
+The default timeout is based on the sensor limit `(4m)`, but we can also define a different timeout, passing the new value in microseconds to the constructor.
 ```python
 from hcsr04 import HCSR04
 
@@ -51,9 +61,29 @@ distance = sensor.distance_cm()
 print('Distance:', distance, 'cm')
 ```
 
+### How to change the air temperature for distance calculation
+
+The `update_air_temp(temp)` method validates whether the new input temp is within sensor's usable range `(-15°C to 70°C)` or not using it. The updated value must be in **Celcius**. By default, the temperature used for calculation is set to 20°C.
+```python
+import utime
+from hcsr04 import HCSR04
+
+sensor = HCSR04(trigger_pin=0, echo_pin=1, air_temp=25)
+sensor.update_air_temp(26)
+
+while True:
+    try:
+        distance_cm = sensor.distance_cm()
+        distance_mm = sensor.distance_mm()
+        print('Distance (cm/mm):', distance_cm, 'cm;\t', distance_mm, 'mm')
+    except Exception as ex:
+        print('ERROR getting distance:', ex)
+    utime.sleep(2)
+```
+
 ### Error management
 
-When the driver reaches the timeout while is listening the echo pin the error is converted to `OSError('Out of range')`
+When the driver reaches the timeout while is listening the echo pin the error is converted to `Exception('Out of range')`
 
 ```python
 from hcsr04 import HCSR04
@@ -63,7 +93,10 @@ sensor = HCSR04(trigger_pin=16, echo_pin=0, echo_timeout_us=10000)
 try:
     distance = sensor.distance_cm()
     print('Distance:', distance, 'cm')
-except OSError as ex:
+except Exception as ex:
     print('ERROR getting distance:', ex)
 
 ```
+
+## List of confirmed supported devices so far:
+- Raspberry Pi Pico
--- hcsr04.py
+++ hcsr04.py
@@ -1,7 +1,8 @@
-import machine, time
-from machine import Pin
+from machine import Pin, time_pulse_us
+from utime import sleep_us
+from math import sqrt
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 __author__ = 'Roberto Sánchez'
 __license__ = "Apache License 2.0. https://www.apache.org/licenses/LICENSE-2.0"
 
@@ -9,19 +10,29 @@ class HCSR04:
     """
     Driver to use the untrasonic sensor HC-SR04.
     The sensor range is between 2cm and 4m.
-
     The timeouts received listening to echo pin are converted to OSError('Out of range')
-
     """
     # echo_timeout_us is based in chip range limit (400cm)
-    def __init__(self, trigger_pin, echo_pin, echo_timeout_us=500*2*30):
+    # air_temp is the temperature of the air. It is setted by default to 20°C
+    # air_temp must be defined in order to calculate the speed of sound in the air. It must be passed in Celsius
+    def __init__(self, trigger_pin, echo_pin, echo_timeout_us=500*2*30, air_temp=20):
         """
         trigger_pin: Output pin to send pulses
         echo_pin: Readonly pin to measure the distance. The pin should be protected with 1k resistor
         echo_timeout_us: Timeout in microseconds to listen to echo pin. 
         By default is based in sensor limit range (4m)
         """
+        # the working temperature of the sensor is between -15°C and 70°C according to the datasheet
+        self.max_working_temp = 70
+        self.min_working_temp = -15
+
+        # speed of sound in the air depends by the temperature of air
+        # so first is checked if the air temp is in the working range of the sensor, then will be calculated the speed of sound
+        # speed of sound is needed to calculate the distance from the sensor to the object (formula is distance = speed of sound/time)
+        self.air_temperature = self._check_air_temp(air_temp)
+        self.sound_speed = self._get_sound_speed()
         self.echo_timeout_us = echo_timeout_us
+
         # Init trigger pin (out)
         self.trigger = Pin(trigger_pin, mode=Pin.OUT, pull=None)
         self.trigger.value(0)
@@ -29,50 +40,85 @@ class HCSR04:
         # Init echo pin (in)
         self.echo = Pin(echo_pin, mode=Pin.IN, pull=None)
 
+    def _check_air_temp(self, temperature):
+        """
+        Check if air temp is in the working range of the sensor
+        """
+        try:
+            if self.min_working_temp>temperature or self.max_working_temp<temperature:
+                raise Exception('temperature out of range')
+            return temperature
+        except Exception as ex:
+            raise
+
+    def _get_sound_speed(self):
+        """
+        Calculate speed of sound in the air (which depends by air temperature)
+        and divide for 1000 in order to convert the speed from m/s to mm/microseconds
+        """
+        ss = 20.05 * (sqrt(self.air_temperature + 273.15))
+        ss = ss / 1000 # conversion from meters / seconds in millimeters / microseconds
+        return ss
+
     def _send_pulse_and_wait(self):
         """
         Send the pulse to trigger and listen on echo pin.
         We use the method `machine.time_pulse_us()` to get the microseconds until the echo is received.
         """
-        self.trigger.value(0) # Stabilize the sensor
-        time.sleep_us(5)
-        self.trigger.value(1)
-        # Send a 10us pulse.
-        time.sleep_us(10)
+        # Stabilize the sensor
         self.trigger.value(0)
+        sleep_us(5)
+        self.trigger.value(1)
+
+        # Send a 10us pulse.
+        sleep_us(10)
+        self.trigger.value(0)
+
         try:
-            pulse_time = machine.time_pulse_us(self.echo, 1, self.echo_timeout_us)
+            pulse_time = time_pulse_us(self.echo, 1, self.echo_timeout_us)
+            if pulse_time < 0: # Already impossible, since the minimum should be 2cm
+                raise Exception('out of range')
             return pulse_time
-        except OSError as ex:
-            if ex.args[0] == 110: # 110 = ETIMEDOUT
-                raise OSError('Out of range')
-            raise ex
+        except Exception as ex:
+            raise
+
+    def update_air_temp(self, temperature):
+        """
+        Check if air temp is in the working range of the sensor
+        """
+        try:
+            if self.min_working_temp>temperature or self.max_working_temp<temperature:
+                raise Exception('temperature out of range')
+            self.air_temperature = temperature
+        except Exception as ex:
+            raise
 
     def distance_mm(self):
         """
-        Get the distance in milimeters without floating point operations.
+        Get the distance in milimeters without floating point operations.(?)
+        To calculate the distance we get the pulse_time and divide it by 2
+        It returns an int
         """
         pulse_time = self._send_pulse_and_wait()
-
-        # To calculate the distance we get the pulse_time and divide it by 2 
-        # (the pulse walk the distance twice) and by 29.1 becasue
-        # the sound speed on air (343.2 m/s), that It's equivalent to
-        # 0.34320 mm/us that is 1mm each 2.91us
-        # pulse_time // 2 // 2.91 -> pulse_time // 5.82 -> pulse_time * 100 // 582 
-        mm = pulse_time * 100 // 582
+        mm = int( (self.sound_speed * pulse_time) // 2 )
         return mm
 
     def distance_cm(self):
         """
         Get the distance in centimeters with floating point operations.
+        To calculate the distance we get the pulse_time and divide it by 20
         It returns a float
         """
         pulse_time = self._send_pulse_and_wait()
-
-        # To calculate the distance we get the pulse_time and divide it by 2 
-        # (the pulse walk the distance twice) and by 29.1 becasue
-        # the sound speed on air (343.2 m/s), that It's equivalent to
-        # 0.034320 cm/us that is 1cm each 29.1us
-        cms = (pulse_time / 2) / 29.1
+        cms = (self.sound_speed * pulse_time) / 20
         return cms
 
+    def distance_in(self):
+        """
+        Get the distance in inches with floating point operations.
+        To calculate the distance we get the pulse_time and divide it by 50.8 (1in = 2.54cm)
+        It returns a float
+        """
+        pulse_time = self._send_pulse_and_wait()
+        inches = (self.sound_speed * pulse_time) / 50.8
+        return inches
